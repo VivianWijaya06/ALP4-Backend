ffaadeb018a81500a33f8e5ac6b0775d4280453f
Fernandes Howard <fernandeshoward2005@gmail.com>
2025-06-11 21:44:37 +0800

    Adding Recipe Features (WIP)

--- prod/src/main/java/com/cookeasy/prod/controller/UserController.java
+++ prod/src/main/java/com/cookeasy/prod/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -63,5 +65,10 @@ public class UserController {
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User tidak ditemukan");
     }
-
+    @GetMapping("/email/{email}")
+    public ResponseEntity<?> getUserByEmail(@PathVariable String email) {
+        User user = userRepository.findByEmail(email);
+        if (user != null) return ResponseEntity.ok(user);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User tidak ditemukan");
+    }
 }
